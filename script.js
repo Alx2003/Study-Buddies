49f5751ed7ae3c1c1d7873026a3cc21f73ccf65c
Alexander Lay <alexanderqlay@gmail.com>
2021-02-21 07:07:54 -0500

    hope

--- script.js
+++ script.js
@@ -181,9 +181,14 @@ function randomNum(max){
   return Math.floor(Math.random() * Math.floor(max));
 }
 
+"username" = document.cookie;
+function getCookie("username") {
+  return "username";
+}
+
 function checkCookie() {
   var cookie = getCookie("username");
-  if (username != "") {
+  if (username == "" || username == null) {
     window.location.replace("signup.html"); //Change to live link
   } else {
       //Read users data from database using "username"
